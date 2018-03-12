aeb62c2cf016fa402e302dacb8b0d237e3acbedf
angietep <35610800+angietep@users.noreply.github.com>
2018-03-12 01:06:26 -0300

    LFP not working

--- Project.py
+++ Project.py
@@ -444,7 +444,7 @@ for i in range(len(postsynList)):
 
 simConfig.dt = 0.05                 # Internal integration timestep to use
 simConfig.verbose = 0
-simConfig.duration = 2.0e3
+simConfig.duration = 1.0e3
 simConfig.recordStim = True
 simConfig.recordStep = 0.1             # Step size in ms to save data (e.g. V traces, LFP, etc)
 
@@ -459,13 +459,14 @@ simConfig.saveDataInclude=['simData']
 #simConfig.saveJson=True
 #simConfig.saveMat=True
 
+sim.createSimulateAnalyze(netParams, simConfig)
+
 simConfig.recordLFP = [[netParams.sizeX/2, netParams.sizeY*1/4, netParams.sizeZ/2],
 						[netParams.sizeX/2, netParams.sizeY*2/4, netParams.sizeZ/2],
 						[netParams.sizeX/2, netParams.sizeY*3/4, netParams.sizeZ/2]]
 
 simConfig.recordLFP = [[x,y,z] for x in range(900, netParams.sizeX, 900) for y in range(40, netParams.sizeY, 40) for z in range(40, netParams.sizeZ, 40)]
 
-sim.createSimulateAnalyze(netParams, simConfig)
 
 sim.analysis.plotConn(graphType='matrix', saveFig=1)
 sim.analysis.plotConn(graphType='bar', saveFig=1)
